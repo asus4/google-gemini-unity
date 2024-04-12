5ab0702fd8eaaa5aed8733d2ed01aac2c40100bb
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-04-12 14:10:08 +0200

    Keep json serializer to reduce creating new object each Json serlialization

--- Packages/Gemini/Runtime/JsonExtensions.cs
+++ Packages/Gemini/Runtime/JsonExtensions.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace Gemini
@@ -6,23 +9,33 @@ namespace Gemini
     internal static class JsonExtensions
     {
         // Remove null values from the JSON 
-        private static readonly JsonSerializerSettings Settings = new()
+        private static readonly JsonSerializerSettings settings = new()
         {
             NullValueHandling = NullValueHandling.Ignore,
             MissingMemberHandling = MissingMemberHandling.Ignore
         };
+        private static readonly JsonSerializer serializer = JsonSerializer.CreateDefault(settings);
+        private static readonly StringBuilder sb = new(256);
 
-        public static string SerializeToJson<T>(this T requestBody, bool prettyPrint = false)
+        public static string SerializeToJson<T>(this T value, bool prettyPrint = false)
         {
+            sb.Clear();
+
             Formatting formatting = prettyPrint
                 ? Formatting.Indented
                 : Formatting.None;
-            return JsonConvert.SerializeObject(requestBody, formatting, Settings);
+            using StringWriter stringWriter = new(sb, CultureInfo.InvariantCulture);
+            using JsonTextWriter jsonTextWriter = new(stringWriter);
+            jsonTextWriter.Formatting = formatting;
+
+            serializer.Formatting = formatting;
+            serializer.Serialize(jsonTextWriter, value, value.GetType());
+            return stringWriter.ToString();
         }
 
         public static T DeserializeFromJson<T>(this string json)
         {
-            T obj = JsonConvert.DeserializeObject<T>(json, Settings)
+            T obj = serializer.Deserialize<T>(new JsonTextReader(new StringReader(json)))
                 ?? throw new Exception("Failed to deserialize JSON");
             return obj;
         }
