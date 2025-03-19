378dbd3d54fabd99c3e910967004f3ce413941e4
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-03-19 09:00:21 +0100

    WIP ContentItem

--- Assets/UI/Components/ContentItem.cs
+++ Assets/UI/Components/ContentItem.cs
@@ -1,22 +1,44 @@
 using System;
+using System.Linq;
 using GoogleApis.GenerativeLanguage;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Unity.Properties;
 
 namespace GoogleApis.Example.UI.Components
 {
     [Serializable]
-    public partial class ContentItem
+    public class ContentItem
     {
-        private Content content;
-        public Content Content
-        {
-            get => content;
-            set => content = value;
-        }
+        [field: SerializeField, DontCreateProperty]
+        public Role Role { get; set; } = Role.model;
 
         [field: SerializeField, DontCreateProperty]
         [CreateProperty]
         public string Text { get; set; }
+
+        [CreateProperty]
+        public FlexDirection ContainerFlexDirection
+        {
+            get
+            {
+                return Role switch
+                {
+                    Role.user => FlexDirection.RowReverse,
+                    Role.model => FlexDirection.Row,
+                    _ => FlexDirection.Column,
+                };
+            }
+        }
+
+        public ContentItem()
+        {
+        }
+
+        public ContentItem(Content content)
+        {
+            Role = content.Role ?? Role.model;
+            Text = content.Parts.First().Text;
+        }    
     }
 }
